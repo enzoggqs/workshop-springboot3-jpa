e9613179cb766bd048d28cb30cb99009b0978b70
Enzo Gabriel Guedes <eggqsaraiva@gmail.com>
2024-01-19 17:55:10 -0300

    Order, Instant, ISO 8601 - part 2

--- src/main/java/com/educandoweb/course/entites/Order.java
+++ src/main/java/com/educandoweb/course/entites/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +22,9 @@ public class Order implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
 	@ManyToOne
@@ -31,18 +35,17 @@ public class Order implements Serializable {
 		
 	}
 
-	public Order(Integer id, Instant moment, User client) {
-		super();
+	public Order(Long id, Instant moment, User client) {
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
